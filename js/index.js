62c4b6a7015c833ca495ea98aad354274e0d3009
Charlene0843 <charlene0843@gmail.com>
2023-04-08 15:56:46 +0800

    集大成

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var scroll_overlay=document.getElementsByClassName('overlay');
+scroll_overlay.addEventListner(scroll,function(){
+
+})
+
+let top_button = getElementsByClassName('top');
+top.addEventListener(click,function(){
+
+})
